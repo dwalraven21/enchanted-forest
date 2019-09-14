3b615b5d0a221210ab40537bdffab7c5e2e44895
Dany Walraven <dwalraven21@gmail.com>
2019-09-14 17:08:26 -0400

    added shuffle deck function and animation on start new game button

--- public/js/app.js
+++ public/js/app.js
@@ -1,102 +1,119 @@
 //temporarily adding cards data here
 const cards = [
 {
+	id: 1,
 	name: "The Beast",
 	front: "images/beast.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 2,
 	name: "The Mermaid",
 	front: "images/bride-of-the-sea.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 3,
 	name: "The Flower Queen",
 	front: "images/flower-queen.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 4,
 	name: "The Frog Prince",
 	front: "images/frog-prince.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 5,
 	name: "The Glass Slipper",
 	front: "images/glass-slipper.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 6,
 	name: "The Lady In White",
 	front: "images/lady-in-white.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 7,
 	name: "The Night Owl",
 	front: "images/night-owl.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 8,
 	name: "Persephone",
 	front: "images/persephone.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 9,
 	name: "The Walraven",
 	front: "images/raven-flight.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 10,
 	name: "The Seelie Queen",
 	front: "images/seelie-queen.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 11,
 	name: "The Shield Maiden",
 	front: "images/shield-maiden.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 12,
 	name: "The Swan Princess",
 	front: "images/swan-princess.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 13,
 	name: "The Golden Ball",
 	front: "images/the-golden-ball.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 14,
 	name: "The White Dove",
 	front: "images/white-dove.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 15,
 	name: "The White Stag",
 	front: "images/white-stag.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 16,
 	name: "Mother Willow",
 	front: "images/willow.jpg",
 	back: "images/back-of-card.png",
 	flipped: false
 },
 {
+	id: 17,
 	name: "The Wolf",
 	front: "images/wolf.jpg",
 	back: "images/back-of-card.png",
@@ -176,11 +193,22 @@ new Vue({
 		newGame: function(){
 
 		},
+		shuffleDeck: function() {
+	        for(let i = this.cards.length - 1; i > 0; i--) {
+		        let randomIndex = Math.floor(Math.random() * i);
+
+		        let temp = this.cards[i];
+		        Vue.set(this.cards, i, this.cards[randomIndex]);
+		        Vue.set(this.cards, randomIndex, temp);
+			}
+		},
+
 	    flipCard: function(card){
 	      card.flipped = !card.flipped;
-		  this.message = "You found " + card.name + "."
-		  kingCard.name == card.name ? this.message2 = "You win the king card!" : this.message2 = "Sorry, it's not a match."
+		  this.message = "You found: " + card.name + "."
+		  kingCard.name == card.name ? this.message2 = "You win the King Card!" : this.message2 = "Sorry, it's not a match."
 		},
+
 		checkForMatch: function(card) {
 			kingCard.name === event.target
 		}
