edd0fb455101130e2fc672f8ffb9b63e942568bd
Dany Walraven <dwalraven21@gmail.com>
2019-09-13 23:12:27 -0400

    layed out game board and scoreboards static for now

--- models/seed.js
+++ models/seed.js
@@ -1,78 +1,78 @@
 module.exports = [
 	{
 		name: "The Beast",
-	    front: "../public/images/beast.jpg",
-	    back: "../public/images/lost.jpg"
+	    front: "images/beast.jpg",
+	    back: "images/lost.jpg"
 	},
 	{
 		name: "The Mermaid",
-		front: "../public/images/bride-of-the-sea.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/bride-of-the-sea.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Flower Queen",
-	    front: "../public/images/flower-queen.jpg",
-	    back: "../public/images/lost.jpg"
+	    front: "images/flower-queen.jpg",
+	    back: "images/lost.jpg"
 	},
 	{
 		name: "The Frog Prince",
-		front: "../public/images/frog-prince.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/frog-prince.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Glass Slipper",
-	    front: "../public/images/glass-slipper.jpg",
-	    back: "../public/images/lost.jpg"
+	    front: "images/glass-slipper.jpg",
+	    back: "images/lost.jpg"
 	},
 	{
 		name: "The Lady In White",
-		front: "../public/images/lady-in-white.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/lady-in-white.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Night Owl",
-		front: "../public/images/night-owl.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/night-owl.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "Persephone",
-		front: "../public/images/persephone.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/persephone.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Walraven",
-		front: "../public/images/raven-flight.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/raven-flight.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Seelie Queen",
-		front: "../public/images/seelie-queen.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/seelie-queen.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Shield Maiden",
-		front: "../public/images/shield-maiden.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/shield-maiden.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Swan Princess",
-		front: "../public/images/swan-princess.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/swan-princess.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The Golden Ball",
-		front: "../public/images/the-golden-ball.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/the-golden-ball.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The White Dove",
-		front: "../public/images/white-dove.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/white-dove.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "The White Stag",
-		front: "../public/images/white-stag.jpg",
-		back: "../public/images/lost.jpg"
+		front: "images/white-stag.jpg",
+		back: "images/lost.jpg"
 	},
 	{
 		name: "Mother Willow",
