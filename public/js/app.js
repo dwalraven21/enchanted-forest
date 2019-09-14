e03d39ba9262c56319ca70571328ab9ff4290525
Dany Walraven <dwalraven21@gmail.com>
2019-09-14 14:37:46 -0400

    updated back of card design

--- public/js/app.js
+++ public/js/app.js
@@ -3,103 +3,103 @@ const cards = [
 {
 	name: "The Beast",
 	front: "images/beast.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Mermaid",
 	front: "images/bride-of-the-sea.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Flower Queen",
 	front: "images/flower-queen.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Frog Prince",
 	front: "images/frog-prince.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Glass Slipper",
 	front: "images/glass-slipper.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Lady In White",
 	front: "images/lady-in-white.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Night Owl",
 	front: "images/night-owl.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "Persephone",
 	front: "images/persephone.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Walraven",
 	front: "images/raven-flight.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Seelie Queen",
 	front: "images/seelie-queen.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Shield Maiden",
 	front: "images/shield-maiden.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Swan Princess",
 	front: "images/swan-princess.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Golden Ball",
 	front: "images/the-golden-ball.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The White Dove",
 	front: "images/white-dove.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The White Stag",
 	front: "images/white-stag.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "Mother Willow",
 	front: "images/willow.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 },
 {
 	name: "The Wolf",
 	front: "images/wolf.jpg",
-	back: "images/lost.jpg",
+	back: "images/back-of-card.png",
 	flipped: false
 }
 ];
@@ -109,7 +109,14 @@ const cards = [
 const players = [
 	{
 		name: 'Player 1',
-		hand: [],
+		hand: [
+			{
+			name: "The Golden Ball",
+			front: "images/the-golden-ball.jpg",
+			back: "images/back-of-card.png",
+			flipped: false
+		}
+	],
 		score: 0,
 		leader: false
 	},
@@ -164,6 +171,9 @@ new Vue({
 	methods: {
 	    toggleCard: function(card){
 	      card.flipped = !card.flipped;
-	    }
+	  },
+	  addToHand: function(card){
+
+	  }
 	}
 })
